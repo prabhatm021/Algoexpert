6c9ef2931c9c80d967e25fa25dce752166b38d6c
prabhatm021 <84736293+prabhatm021@users.noreply.github.com>
2022-08-05 15:56:00 +0530

    added the solution for Problem - Two Number Sum

---
+++ Two_Number_Sum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+#include <vector>
+using namespace std;
+
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+  // Write your code here.
+  //sort the array 
+  int start = 0; // starting index of array
+  int end = array.size() -1 ; // ending index of array
+  sort(array.begin(),array.end()); // sorting of the array 
+  while(start < end){  // adding elements to check with the targetSum 
+    if(array[start] + array[end] < targetSum){  // checking each case 
+      start = start + 1;
+    }
+    if(array[start] + array[end] > targetSum){
+      end = end -1 ;
+    }
+    if(array[start] + array[end] == targetSum){
+      return {array[start], array[end]}; // result
+    }
+  }
+  return {}; // if the while loop is skipped we return empty array
+}
